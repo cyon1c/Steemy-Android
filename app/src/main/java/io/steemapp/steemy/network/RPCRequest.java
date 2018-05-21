02a776c3cddb920d43ada6fb55548e7302045089
John White <john@steemit.com>
2018-05-21 16:12:46 -0500

    Added RPCRequest object and LoggingInterceptor. OkHttp3 was added to the project to give more fine grain control over the HttpClient.

---
+++ app/src/main/java/io/steemapp/steemy/network/RPCRequest.java
@@ -0,0 +1,56 @@
+package io.steemapp.steemy.network;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Random;
+
+public class RPCRequest {
+
+    private static Random idGenerator = new Random();
+    private static Integer idIncremental = idGenerator.nextInt();
+
+    @SerializedName("id")
+    private Integer id;
+
+    @SerializedName("params")
+    private Object[] params;
+
+    @SerializedName("method")
+    private String method;
+
+    public RPCRequest(Integer id) {
+        this.id = id;
+    }
+
+    public static RPCRequest emptySequentialRequest(){
+        return new RPCRequest(idIncremental++);
+    }
+
+    public static RPCRequest emptyRequest(){
+        return new RPCRequest(idGenerator.nextInt());
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Object[] getParams() {
+        return params;
+    }
+
+    public void setParams(Object[] params) {
+        this.params = params;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+}
--- app/src/main/java/io/steemapp/steemy/network/SteemyNetworkAdapter.java
+++ app/src/main/java/io/steemapp/steemy/network/SteemyNetworkAdapter.java
@@ -1,5 +1,7 @@
 package io.steemapp.steemy.network;
 
+import okhttp3.Cache;
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -8,13 +10,28 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 public class SteemyNetworkAdapter {
 
-    private final static String BASEURL = "http://www.steemapp.io/";
+    private final static String BASEURL = "https://api.steemit.com/";
 
     protected static SteemyRetrofitService getNewService() {
         return new Retrofit.Builder()
                 .baseUrl(BASEURL)
+                .client(newJsonRPCClient())
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
                 .create(SteemyRetrofitService.class);
     }
+
+    private static OkHttpClient newJsonRPCClient(){
+//        OkHttpClient clientBuilder = new OkHttpClient.Builder()
+//                .addInterceptor(new LoggingInterceptor())
+//                .cache(buildCache());
+
+        return new OkHttpClient();
+    }
+
+//    private static Cache buildCache(){
+//
+//    }
+
+
 }
